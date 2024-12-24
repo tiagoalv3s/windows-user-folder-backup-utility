82042ecb931bd103c5bfbde96e387867fc0a956f
tiagoalv3s <tiagoalv3s@hotmail.com>
2024-12-24 19:48:54 -0300

    Basic directory structure

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import shutil
+
+main_path = Path.home()
+
+directories = [ 
+    'Downloads', 
+    'Documents', 
+    'Desktop', 
+    'Pictures', 
+    'Videos', 
+    'Music', 
+    'Other'
+    ]
+
