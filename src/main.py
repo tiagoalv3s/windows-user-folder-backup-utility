a2d4bc1ecb1e2e6f08bb0b9f6defc3d409bd0259
tiagoalv3s <tiagoalv3s@hotmail.com>
2024-12-26 15:48:40 -0300

    Add copying backup files to propper folders

--- src/main.py
+++ src/main.py
@@ -17,4 +17,7 @@ for item in directories:
     if not Path(backups/item).is_dir():
         Path(backups/item).mkdir(parents=True, exist_ok=True)
 
-
+for i in range(len(directories)):
+    for file in Path(main_path/directories[i]).glob('**/*'):
+        if file.is_file() and file.suffix not in ['.ini']:
+            shutil.copy(file, backups/directories[i])
